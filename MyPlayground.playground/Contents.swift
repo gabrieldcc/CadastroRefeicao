5d7d568f4f4aced30a0aa30e80ef7a87b1972fe0
Gabriel de Castro <gabrieldcc@gmail.com>
2022-07-18 18:08:22 -0300

    subindo projeto para o github

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,1 +1,20 @@
+var numbers: [Int] = []
+
+func inputNumbers(quantityOfNumbers: Int) {
+    for n in 1...quantityOfNumbers {
+        numbers.append(n)
+    }
+}
+
+func sumNumbers() -> Int {
+    var total = 0
+    for n in numbers {
+        total += n
+    }
+    return total
+}
+
+inputNumbers(quantityOfNumbers: 100)
+print(numbers)
+print(sumNumbers())
 
